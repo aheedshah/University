a6f69afcd7e751263de1be558662a2470c507ee3
Aheed Shah <74835027+aheedshah@users.noreply.github.com>
2022-01-08 18:30:27 +0000

    Update HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class HashTable {
-    private int[] buckets;
+    public int[] buckets;
     public long a; //The constant for the equation
     public long c; //Another constant for the equation
     public int m; //Number of buckets of the hashtable.
