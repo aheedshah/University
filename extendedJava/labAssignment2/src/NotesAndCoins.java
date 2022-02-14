04903cf4ccffb02ab3614e9356f00cda5e583571
Aheed Shah <aheed@duck.com>
2022-02-14 15:03:39 +0000

    Initial Commit

---
+++ extendedJava/labAssignment2/src/NotesAndCoins.java
@@ -0,0 +1,11 @@
+public enum NotesAndCoins {
+
+
+    //how many pence is each constant worth
+    private int valueInP;
+    //a user friendly name for the constant
+    private String name;
+
+    //add constructors and getters for valueInP and names
+
+}
